3763082b962a5a2c5af4c44e701e1bf8a9064b11
Cole Cofer <coleocofer@gmail.com>
2019-05-27 21:39:10 -0700

    Save point before rebasing master

--- AI_Tower_Defense/src/constants/gameConstants.py
+++ AI_Tower_Defense/src/constants/gameConstants.py
@@ -18,7 +18,7 @@ from towers.igloo import Igloo
 #Fullscreen will make the game run waaaay better
 FULLSCREEN_MODE = False
 PLAY_BG_MUSIC = True        #Set false to turn music off
-SHOW_MOUSE_CLICKS = False   #If true will display dots where clicked, and print the coordinate in terminal
+SHOW_MOUSE_CLICKS = True   #If true will display dots where clicked, and print the coordinate in terminal
 SHOW_PATH_BOUNDS = True     #If true will display bounds to enemy path
 
 TOWER_POSITIONS = [(35, 294), (131, 289), (128, 181), (189, 151), (354, 150), (428, 387), (492, 383), (493, 261), (423, 264), (559, 211), (732, 207), (279, 302), (277, 380), (44, 427), (193, 430), (355, 519), (468, 517), (591, 516), (657, 351), (679, 412), (637, 416), (822, 341), (817, 285), (904, 182), (1152, 180), (1034, 180), (1160, 321), (1072, 320), (990, 321), (972, 422), (282, 458), (272, 149), (645, 209), (425, 200), (127, 233), (747, 458), (899, 455)]
