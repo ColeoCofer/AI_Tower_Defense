4f2fced207d8504e1457cde7d05813c348523b89
Cole Cofer <coleocofer@gmail.com>
2019-05-27 21:51:24 -0700

    Actually finished merge conflicts

--- AI_Tower_Defense/src/ui/menu.py
+++ AI_Tower_Defense/src/ui/menu.py
@@ -1,38 +1,10 @@
 import pygame
 from .towerButton import TowerButton
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> Now selects tower and draws it at mouse position
-=======
->>>>>>> 3763082b962a5a2c5af4c44e701e1bf8a9064b11
 HEIGHT_GAP_PX = 4     #Distance from top of background rect
 WIDTH_GAP_PX = 40     #How "spread out" the tower buttons are from each other
 IMG_SIZE = (60, 60)   #Size of tower buttons
 BOTTOM_PX = 40        #Area where name and cost are displayed
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-GAP_PX = 2
-IMG_SIZE = (60, 60)
->>>>>>> Got the basic menu displaying
-=======
-GAP_PX = 1
-=======
-HEIGHT_GAP_PX = 4
-WIDTH_GAP_PX = 40
->>>>>>> Got the menu looking much better
-IMG_SIZE = (60, 60)
-BOTTOM_PX = 40 #Area where name and cost is
->>>>>>> Got the name and cost displayed but it needs some tweaking
-=======
->>>>>>> Now selects tower and draws it at mouse position
-=======
->>>>>>> 3763082b962a5a2c5af4c44e701e1bf8a9064b11
 
 class Menu:
     ''' Creates a purchase menu of tower buttons '''
@@ -42,11 +14,6 @@ class Menu:
 
         #Create a button for every tower
         totalSizeX = 0
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 3763082b962a5a2c5af4c44e701e1bf8a9064b11
         i = 0
         for i in range(len(towers)):
             if i == -1:
@@ -59,11 +26,6 @@ class Menu:
             #Create a dummy tower object to get the data members
             tower = towers[i]((0, 0))
             resizedTowerImage = pygame.transform.scale(tower.image, IMG_SIZE)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 3763082b962a5a2c5af4c44e701e1bf8a9064b11
             self.buttons.append(TowerButton((buttonPositionX, self.position[1] + HEIGHT_GAP_PX), IMG_SIZE, resizedTowerImage, tower.name, tower.cost, towers[i]))
 
         self.width = (len(towers) * (IMG_SIZE[0] + WIDTH_GAP_PX)) - WIDTH_GAP_PX
@@ -71,66 +33,6 @@ class Menu:
         self.bgRect = pygame.Surface((self.width, self.height))
         self.bgRect.set_alpha(220)
         self.bgRect.fill((137, 139, 145))
-<<<<<<< HEAD
-=======
-        for tower in towers:
-            buttonPosition = position + GAP + lastImgX
-=======
-        i = 0
-        for i in range(len(towers)):
-<<<<<<< HEAD
-<<<<<<< HEAD
-            buttonPosition = position[0] + GAP_PX + lastImgX
-            tower = towers[i]((0, 0))
->>>>>>> Got the basic menu displaying
-=======
-            # buttonPosition = position[0] + GAP_PX + lastImgX        #Add a gap between each image
-            buttonPosition = i * IMG_SIZE[0] + GAP_PX + self.position[0]
-            tower = towers[i]((0, 0))                               #Create a dummy tower object to get the data members
->>>>>>> Got the name and cost displayed but it needs some tweaking
-=======
-            if i == -1:
-                #Position the first image at the front
-                buttonPositionX = self.position[0] + WIDTH_GAP_PX
-            else:
-                #Position consecutive buttons after each other
-                buttonPositionX = (i * (IMG_SIZE[0] + (WIDTH_GAP_PX))) + self.position[0]
-
-            #Create a dummy tower object to get the data members
-            tower = towers[i]((0, 0))
->>>>>>> Got the menu looking much better
-            resizedTowerImage = pygame.transform.scale(tower.image, IMG_SIZE)
-            self.buttons.append(TowerButton((buttonPositionX, self.position[1] + HEIGHT_GAP_PX), IMG_SIZE, resizedTowerImage, tower.name, tower.cost))
-=======
-
-=======
->>>>>>> Fixed placement issue
-            self.buttons.append(TowerButton((buttonPositionX, self.position[1] + HEIGHT_GAP_PX), IMG_SIZE, resizedTowerImage, tower.name, tower.cost, towers[i]))
->>>>>>> Got them placinggit statusgit status
-
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> Save point before running
-=======
-        # self.width = (IMG_SIZE[0] *  + (len(towers) * GAP_PX)
-=======
->>>>>>> Lots of progress with path bounds
-        self.width = (len(towers) * (IMG_SIZE[0] + WIDTH_GAP_PX)) - WIDTH_GAP_PX
-        self.height = IMG_SIZE[1] + HEIGHT_GAP_PX + BOTTOM_PX
-        self.bgRect = pygame.Surface((self.width, self.height))
-<<<<<<< HEAD
-<<<<<<< HEAD
-        self.bgRect.fill((234, 209, 161))
->>>>>>> Got the name and cost displayed but it needs some tweaking
-=======
-        self.bgRect.set_alpha(200)
-=======
-        self.bgRect.set_alpha(220)
->>>>>>> Added color variance in health text
-        self.bgRect.fill((137, 139, 145))
->>>>>>> Got the menu looking much better
-=======
->>>>>>> 3763082b962a5a2c5af4c44e701e1bf8a9064b11
 
     def draw(self, win):
         ''' Draws the tower buttons over the background rect '''
@@ -139,32 +41,16 @@ class Menu:
         win.blit(self.bgRect, self.position)
 
         #Render the buttons over the background
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 3763082b962a5a2c5af4c44e701e1bf8a9064b11
         for button in self.buttons:
             button.draw(win)
 
 
     def handleEvents(self, mousePosition, wallet, pathBounds):
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> its pretty ballin
-=======
->>>>>>> 3763082b962a5a2c5af4c44e701e1bf8a9064b11
         '''
         Handle if the user selects a tower button
         Returns the tower type if a user selected one for purchasing
         '''
         buttonWasSelected = False
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 3763082b962a5a2c5af4c44e701e1bf8a9064b11
         i = 0
         for i in range(len(self.buttons)):
             isSelected, towerType = self.buttons[i].handleEvents(mousePosition, wallet, pathBounds)
@@ -185,65 +71,3 @@ class Menu:
                 return towerType, buttonWasSelected
 
         return towerType, buttonWasSelected
-<<<<<<< HEAD
-=======
-        for button in buttons:
-            button.draw()
->>>>>>> Save point before running
-=======
-        for button in self.buttons:
-            button.draw(win)
-<<<<<<< HEAD
->>>>>>> Got the basic menu displaying
-=======
-
-<<<<<<< HEAD
-    def handleEvents(self, mousePosition):
-        for button in self.buttons:
-            button.handleEvents(mousePosition)
->>>>>>> Got the name and cost displayed but it needs some tweaking
-=======
-
-    def handleEvents(self, mousePosition, wallet):
-=======
->>>>>>> You can actually buy the towers now
-        ''' Handle if the user selects a tower button '''
-=======
->>>>>>> its pretty ballin
-        i = 0
-        for i in range(len(self.buttons)):
-            isSelected, towerType = self.buttons[i].handleEvents(mousePosition, wallet, pathBounds)
-
-            #If they purchased one, deselect all and return the tower to place
-            if isSelected == False and towerType != None:
-                for button in self.buttons:
-                    button.isSelected = False
-                return towerType, buttonWasSelected
-
-            #If we selected a new button, deselect the rest of them
-            if isSelected == True:
-                for j in range(len(self.buttons)):
-                    if j != i:
-                        #Deselect all other buttons
-                        self.buttons[j].isSelected = False
-<<<<<<< HEAD
-<<<<<<< HEAD
-                break
-<<<<<<< HEAD
->>>>>>> Now selects tower and draws it at mouse position
-=======
-        #User has purchased and placed a tower if not None
-=======
-                return towerType
-
->>>>>>> Fixed placement issue
-        return towerType
->>>>>>> Got them placinggit statusgit status
-=======
-                        buttonWasSelected = True
-                return towerType, buttonWasSelected
-
-        return towerType, buttonWasSelected
->>>>>>> its pretty ballin
-=======
->>>>>>> 3763082b962a5a2c5af4c44e701e1bf8a9064b11
--- AI_Tower_Defense/src/ui/towerButton.py
+++ AI_Tower_Defense/src/ui/towerButton.py
@@ -12,40 +12,15 @@ class TowerButton:
         self.title = title
         self.cost = cost
         self.rect = pygame.Rect(self.position, self.size)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 3763082b962a5a2c5af4c44e701e1bf8a9064b11
         self.titleFont = pygame.font.SysFont('lucidagrandettc', 12)
         self.costFont = pygame.font.SysFont('lucidagrandettc', 15)
         self.titleColor = (0, 0, 0)
         self.costColor = (250, 241, 95)
         self.isSelected = False
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-        self.font = pygame.font.SysFont('lucidagrandettc', 10)
-        self.titleColor = (0, 0, 0)
-        self.costColor = (160, 141, 95)
->>>>>>> Got the name and cost displayed but it needs some tweaking
-=======
-        self.titleFont = pygame.font.SysFont('lucidagrandettc', 12)
-        self.costFont = pygame.font.SysFont('lucidagrandettc', 15)
-        self.titleColor = (0, 0, 0)
-        self.costColor = (250, 241, 95)
->>>>>>> Got the menu looking much better
-=======
->>>>>>> Now selects tower and draws it at mouse position
-=======
->>>>>>> 3763082b962a5a2c5af4c44e701e1bf8a9064b11
 
     def draw(self, win):
         ''' Draw the button containing an image of the tower '''
         win.blit(self.image, self.rect)
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
         namePosition = (self.position[0], self.position[1] + self.size[1])
         self.displayText(self.title, namePosition, self.titleColor, win, self.titleFont)
         costPosition = (namePosition[0], namePosition[1] + TEXT_GAP_PX)
@@ -94,84 +69,3 @@ class TowerButton:
         ''' Displays the text at the given position '''
         fontSurface = font.render(text, False, color)
         win.blit(fontSurface, position)
-=======
-=======
-        namePosition = (self.position[0], self.position[1] + self.size[1])
-        self.displayText(self.title, namePosition, self.titleColor, win, self.titleFont)
-        costPosition = (namePosition[0], namePosition[1] + TEXT_GAP_PX)
-<<<<<<< HEAD
-        self.displayText(str(self.cost), costPosition, self.costColor, win)
->>>>>>> Got the name and cost displayed but it needs some tweaking
-=======
-        self.displayText(str(self.cost), costPosition, self.costColor, win, self.costFont)
->>>>>>> Got the menu looking much better
-
-<<<<<<< HEAD
-    def handleEvents(self, mousePosition):
-        ''' Attempts to purchase the tower if the user clicks on it '''
-<<<<<<< HEAD
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if self.rect.collidepoint(event.pos):
-                print(f"You clicked the {self.title} button!")
-                #This is where we'd attempt to but the tower
->>>>>>> Got the basic menu displaying
-=======
-=======
-=======
-        namePosition = (self.position[0], self.position[1] + self.size[1])
-        self.displayText(self.title, namePosition, self.titleColor, win, self.titleFont)
-        costPosition = (namePosition[0], namePosition[1] + TEXT_GAP_PX)
-        self.displayText(str(self.cost), costPosition, self.costColor, win, self.costFont)
-
->>>>>>> 3763082b962a5a2c5af4c44e701e1bf8a9064b11
-        self.isPlacingTower(win)
-
-    def handleEvents(self, mousePosition, wallet, pathBounds):
-        '''
-        Attempts to purchase the tower if the user clicks on it
-        Returns the name of the tower so we can reset all isSelected values in menu
-        '''
-        #Check if they clicked within bounds of the button
-<<<<<<< HEAD
->>>>>>> Now selects tower and draws it at mouse position
-=======
->>>>>>> 3763082b962a5a2c5af4c44e701e1bf8a9064b11
-        if self.rect.collidepoint(mousePosition):
-            #Check if they have enough coins
-            if wallet.coins >= self.cost and self.isSelected == False:
-                self.isSelected = True
-                return True, None
-        #Check if they have already selected a tower, and tried place it at a valid location
-        elif self.isSelected == True and wallet.coins >= self.cost and self.canPlaceTower(pathBounds) == True:
-            wallet.spendCoins(self.cost)
-            #May need to return a tower to be created here...
-            self.isSelected = False
-            return False, self.type
-
-        return False, None
-
-    def canPlaceTower(self, pathBounds):
-        ''' Returns true if the current mouse position is a valid place to build a tower '''
-        mousePosition = pygame.mouse.get_pos()
-        w, h = (self.size[0]/2), (self.size[1]/2)
-        for rect in pathBounds:
-            #If we collide with any of these rectangles, return False
-            x, y = mousePosition[0], mousePosition[1]
-            if rect.collidepoint((x-w, y-h)) or rect.collidepoint((x-w, y+h)) or rect.collidepoint((x+w, y-h)) or rect.collidepoint((x+w, y+h)):
-                return False
-        return True
-
-    def isPlacingTower(self, win):
-        ''' Checks if the user is currently placing a tower and draws it on the mouse position '''
-        if self.isSelected == True:
-            mousePosition = pygame.mouse.get_pos()
-            win.blit(self.image, (mousePosition[0] - (self.size[0] / 2), mousePosition[1] - (self.size[1] / 2)))
-
-    def displayText(self, text, position, color, win, font):
-        ''' Displays the text at the given position '''
-        fontSurface = font.render(text, False, color)
-        win.blit(fontSurface, position)
-<<<<<<< HEAD
->>>>>>> Got the name and cost displayed but it needs some tweaking
-=======
->>>>>>> 3763082b962a5a2c5af4c44e701e1bf8a9064b11
