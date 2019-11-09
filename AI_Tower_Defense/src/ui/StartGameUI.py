f2befc081da668ad7b6ea93754484b306cf2ffc1
Cole Cofer <coleocofer@gmail.com>
2019-11-09 11:19:57 -0800

    Added combobox for AI types

--- AI_Tower_Defense/src/ui/StartGameUI.py
+++ AI_Tower_Defense/src/ui/StartGameUI.py
@@ -1,39 +1,60 @@
-from tkinter import *
+#from tkinter import
+import tkinter as tk
+import tkinter.ttk as tkk
 
 WINDOW_SIZE = '700x500'
 FONT_HEADER = ("Arial Bold", 30)
 FONT_BODY = ("Arial", 15)
-BTN_BG_COLOR = "grey"
-BTN_FG_COLOR = "BLACK"
+BTN_BG_COLOR = "blue"
+BTN_FG_COLOR = "black"
 
 class StartUpWindow():
 
     def __init__(self):
 
         #Window Setup
-        window = Tk()
+        window = tk.Tk()
         window.geometry(WINDOW_SIZE)
         window.title("AI Tower Defense")
 
-        welcomeLabel = Label(window, text="Welcome to: AI Tower Defense!", font=FONT_HEADER)
+        welcomeLabel = tk.Label(window, text="Welcome to: AI Tower Defense!", font=FONT_HEADER)
         welcomeLabel.grid(column=0, row=0)
 
+        #Variables for different radio button groups
+        manualOrAI = tk.IntVar()
+
         def manualRadioBtnSelected():
-            print("Manual")
+            print(f"Manual: {manualOrAI}")
 
         def aiRadioBtnSelected():
-            print("AI ")
+            print(f"AI:     {manualOrAI}")
+
+        def hideElement(event):
+            event.grid_remove()
+
+        def showElement(event):
+            event.grid()
+
+        def startButtonClicked():
+            print('Start button clicked')
+
+        #Start Game Button
+        startGameBtn = tk.Button(window, text="Start Game", command=startButtonClicked)
+        startGameBtn.grid(column=0, row=10)
 
         #Radio Buttons Manual / AI
-        manualRadioBtn = Radiobutton(window, text="Play Game Manually", value=1, command=manualRadioBtnSelected)
-        aiRadioBtn = Radiobutton(window, text="Artifical Intelligence", value=2, command=aiRadioBtnSelected)
+        manualRadioBtn = tk.Radiobutton(window, text="Play Game Manually", value=1,variable=manualOrAI, command=manualRadioBtnSelected, indicatoron=0)
+        aiRadioBtn = tk.Radiobutton(window, text="Artifical Intelligence", value=2, variable=manualOrAI, command=aiRadioBtnSelected, indicatoron=0)
         manualRadioBtn.grid(column=0, row=1)
         aiRadioBtn.grid(column=0, row=2)
 
-        window.mainloop()
+        #AI Settings (none)
+        aiTypeCombo = tkk.Combobox(window)
+        aiTypeCombo['values']= ("Genetic Algorithm", "Q-Learning","Deep Q-Learning")
+        aiTypeCombo.current(1) #set the selected item
+        aiTypeCombo.grid(column=0, row=6)
 
-    #Wire button up like
-    #btn = Button(window, text="", command=clicked)
-    def clicked():
-        #self.label.configure(text="Button clicked!")
-        print("Button clicked!")
+
+
+
+        window.mainloop()
